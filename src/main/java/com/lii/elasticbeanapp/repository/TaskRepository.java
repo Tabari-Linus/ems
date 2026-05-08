f0cf61c7768f53edb99466976bd21438dae2e35d
Linus-Tabari <68056745+Tabari-Linus@users.noreply.github.com>
2026-05-08 10:30:25 +0000

    Delete src directory

--- src/main/java/com/lii/elasticbeanapp/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lii.elasticbeanapp.repository;
-
-import com.lii.elasticbeanapp.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
